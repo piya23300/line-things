92afc8cb00b4ea27ab517113ad781fe783d743b7
PiYa <piya23300@gmail.com>
2019-07-18 14:29:17 +0700

    update service uuid

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '91E4E176-D0B9-464D-9FE4-52EE3E9F1552'; // LED, Button
+const USER_SERVICE_UUID         = 'e92dc89a-2fe5-4f40-900f-39364e0b9e10'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
@@ -17,7 +17,7 @@ let clickCount = 0;
 // -------------- //
 
 window.onload = () => {
-    initializeApp();
+  initializeApp();
 };
 
 // ----------------- //
@@ -25,10 +25,10 @@ window.onload = () => {
 // ----------------- //
 
 function handlerToggleLed() {
-    ledState = !ledState;
+  ledState = !ledState;
 
-    uiToggleLedButton(ledState);
-    liffToggleDeviceLedState(ledState);
+  uiToggleLedButton(ledState);
+  liffToggleDeviceLedState(ledState);
 }
 
 // ------------ //
